1553626909ce3d9b3ce6a6ef872b03caa34a5abb
Emile Kott <emilekott@gmail.com>
2013-09-13 15:27:39 +0100

    more styling changes

--- wp-content/themes/coding-standard/footer.php
+++ wp-content/themes/coding-standard/footer.php
@@ -10,12 +10,19 @@
  * @since Twenty Twelve 1.0
  */
 ?>
-	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'twentytwelve_credits' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+</div><!-- #main .wrapper -->
+<footer id="colophon" role="contentinfo">
+  <div class="site-info">
+    <div class="copyright">
+      Copyright <?php echo date("Y"); ?>, Programming Research Ltd. &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+      <a href="<?php echo home_url(); ?>/privacy-policy">Privacy Policy</a>
+    </div>
+    <div class="sponsored">
+      <a href="http://www.programmingresearch.com/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/sponsored.jpg"</a>
+    </div>
+    <div style ="clear:both"></div>
+  </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
--- wp-content/themes/coding-standard/single-rule.php
+++ wp-content/themes/coding-standard/single-rule.php
@@ -20,18 +20,27 @@ get_header();
    
 
     <?php while (have_posts()) : the_post(); ?>
+    <?php $terms = wp_get_post_terms( $post->ID, 'section'); 
+        //print_r($terms);
+        if ($terms[0]->term_id){
+          $term = $terms[0]->slug;
+          $term_link = home_url().'/section/'.$term;
+          $term_link = '<a href="'.$term_link.'">&uarr; Up</a>';
+                 
+        }?>
 
       <?php get_template_part('content', get_post_format()); ?>
 
       <nav class="nav-single">
         <h3 class="assistive-text"><?php _e('Post navigation', 'twentytwelve'); ?></h3>
         <?php //prev and next have been swapped so that they make sense ?>
-        <span class="nav-previous"><?php next_post_link('%link', '<span class="meta-nav">' . _x('&larr;', 'Previous post link', 'twentytwelve') . '</span> %title '); ?></span> 	
+        <span class="nav-previous"><?php next_post_link('%link', '<span class="meta-nav">' . _x('&larr;', 'Previous post link', 'twentytwelve') . '</span> %title '); ?></span> 
+        <span class="nav-up"><?php echo $term_link; ?></span>
         <span class="nav-next"><?php previous_post_link('%link', '%title <span class="meta-nav">' . _x('&rarr;', 'Next post link', 'twentytwelve') . '</span>'); ?></span>
       </nav><!-- .nav-single -->
       <a name="respond"></a><a name="comments"></a>
       <?php comments_template('', true); ?>
-
+      
     <?php endwhile; // end of the loop. ?>
 
   </div><!-- #content -->
